a682869e637f01d7ae96d93b7bf4247520ce22b5
ArshadBeldar <arshadsk9527@gmail.com>
2022-10-17 00:38:51 +0530

    ADDED: Add C language file Addition Of Two No

---
+++ ADDITION.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int no1,no2,res=0;
+	clrscr();
+	printf("WELLCOME TO GITHUB");
+	printf("\nEnter the Addition Of Two Number");
+	scanf("%d%d",&no1,&no2);
+	res=no1+no2;
+	printf("\nAddition is : %d",res);
+	getch();
+}
